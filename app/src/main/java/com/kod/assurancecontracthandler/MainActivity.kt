25aa569ae0d2afa4bae4987d44fbe3aeb24c23a0
TENE Emma <tenetampo@spreeloop.com>
2023-09-12 23:02:56 +0100

    updated activities

--- app/src/main/java/com/kod/assurancecontracthandler/MainActivity.kt
+++ app/src/main/java/com/kod/assurancecontracthandler/MainActivity.kt
@@ -11,15 +11,16 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.navigation.NavHostController
 import androidx.navigation.fragment.NavHostFragment
+import com.kod.assurancecontracthandler.databinding.ActivityBaseBinding
 import com.kod.assurancecontracthandler.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityBaseBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityBaseBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
