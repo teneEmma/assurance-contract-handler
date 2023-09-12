be0e5f49e62b8dcb991a9384fc097964741c51e5
TENE Emma <tenetampo@spreeloop.com>
2023-09-12 23:14:48 +0100

    removed unsed imports and centered title in toolbar

--- app/src/main/java/com/kod/assurancecontracthandler/MainActivity.kt
+++ app/src/main/java/com/kod/assurancecontracthandler/MainActivity.kt
@@ -1,18 +1,8 @@
 package com.kod.assurancecontracthandler
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import androidx.navigation.NavHostController
-import androidx.navigation.fragment.NavHostFragment
 import com.kod.assurancecontracthandler.databinding.ActivityBaseBinding
-import com.kod.assurancecontracthandler.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
