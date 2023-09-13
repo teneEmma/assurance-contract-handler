089d706644dbc86db2c27631c0150e177b821d00
TENE Emma <tenetampo@spreeloop.com>
2023-09-13 22:01:40 +0100

    removed unwanted changes

--- app/src/androidTest/java/com/kod/assurancecontracthandler/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kod/assurancecontracthandler/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.kod.assurancecontracthandler222", appContext.packageName)
+        assertEquals("com.kod.assurancecontracthandler", appContext.packageName)
     }
 }
