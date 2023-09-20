50e060c40c414b5b032aa39226281ebd4a98789e
TENE Emma <tenetampo@spreeloop.com>
2023-09-20 23:44:11 +0100

    added click listener on contract list page

--- app/src/main/java/com/kod/assurancecontracthandler/views/main/fragmentListContracts/ContractListFragment.kt
+++ app/src/main/java/com/kod/assurancecontracthandler/views/main/fragmentListContracts/ContractListFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.kod.assurancecontracthandler.R
 import com.kod.assurancecontracthandler.databinding.FragmentListContractsBinding
 
 
@@ -27,5 +29,19 @@ class ContractListFragment : Fragment() {
     ): View {
         binding = FragmentListContractsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.addExcelFile.setOnClickListener {
+            openAddExcelFilePage()
+        }
+
+        binding.ivEmptyDatabase.setOnClickListener {
+            openAddExcelFilePage()
+        }
+    }
+
+    private fun openAddExcelFilePage(){
+        findNavController().navigate(R.id.action_HomeFragment_to_SelectFileFragment)
     }
 }
