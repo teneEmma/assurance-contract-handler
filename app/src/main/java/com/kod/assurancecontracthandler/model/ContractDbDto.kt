3c167cc768a687d5c867d2e77f2bba3e796897bc
TENE Emma <tenetampo@spreeloop.com>
2023-09-22 20:07:26 +0100

    contractDbDto model

--- app/src/main/java/com/kod/assurancecontracthandler/model/ContractDbDto.kt
+++ app/src/main/java/com/kod/assurancecontracthandler/model/ContractDbDto.kt
@@ -5,15 +5,26 @@ import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
-import java.util.*
+import java.time.LocalDate
 
+/**
+ * Model representing a 'contract' table in our database.
+ *
+ *
+ * @param id Represents the primary key of each entry in our DB.
+ * @param sheetCreationDateStart This is the first year that appears in the header of our Excel sheet.
+ *  * e.g. sheetCreationDateStart in [2021-2022] is 2021
+ * @param sheetCreationDateEnd This is the second year that appears in the header of our Excel sheet.
+ *  * e.g. sheetCreationDateEnd in [2021-2022] is 2022
+ *  @param contract This is the contract that is going to be saved in the DB.
+ */
 @Entity(tableName = "contract")
 @Parcelize
 data class ContractDbDto(
     @PrimaryKey(autoGenerate = true)
     var id: Int,
-    val sheetCreationDateStart: Date = Date(),
-    val sheetCreationDateEnd: Date = Date(),
+    val sheetCreationDateStart: String = LocalDate.now().toString(),
+    val sheetCreationDateEnd: String = LocalDate.now().toString(),
     @Embedded
     val contract: Contract?
 ): Parcelable
