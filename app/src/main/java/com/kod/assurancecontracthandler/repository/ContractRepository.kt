179599f0c40af81cb1d60cabd4e8abb05bdbd123
Tene Tampo Emmanuel <103233184+teneEmma@users.noreply.github.com>
2023-09-27 23:40:07 +0200

    imported files from base branch (#51)

--- app/src/main/java/com/kod/assurancecontracthandler/repository/ContractRepository.kt
+++ app/src/main/java/com/kod/assurancecontracthandler/repository/ContractRepository.kt
@@ -2,17 +2,17 @@ package com.kod.assurancecontracthandler.repository
 
 import androidx.sqlite.db.SimpleSQLiteQuery
 import com.kod.assurancecontracthandler.dao.ContractDAO
-import com.kod.assurancecontracthandler.model.ContractDbDto
+import com.kod.assurancecontracthandler.model.BaseContract
 
 class ContractRepository(private val contractDao: ContractDAO) {
 
-    suspend fun addContract(contract: ContractDbDto) = contractDao.addContract(contract)
+    suspend fun addContract(contract: BaseContract) = contractDao.addContract(contract)
 
-    suspend fun addContracts(contracts: List<ContractDbDto>) = contractDao.addContracts(contracts)
+    suspend fun addContracts(contracts: List<BaseContract>) = contractDao.addContracts(contracts)
 
-    suspend fun updateContract(contract: ContractDbDto) = contractDao.updateContract(contract)
+    suspend fun updateContract(contract: BaseContract) = contractDao.updateContract(contract)
 
-    suspend fun fetchCustomerContract(customerName: String): List<ContractDbDto> = contractDao.fetchCustomerContract(customerName)
+    suspend fun fetchCustomerContract(customerName: String): List<BaseContract> = contractDao.fetchCustomerContract(customerName)
 
     suspend fun updateCustomer(oldName: String, customerName: String, phoneNumber: String) =
         contractDao.updateContract(oldName = oldName,
@@ -23,6 +23,6 @@ class ContractRepository(private val contractDao: ContractDAO) {
 
     fun searchForClient(query: SimpleSQLiteQuery) = contractDao.searchForAClient(query)
 
-    fun getExpiringContractsIn(today: Long, maxTime: Long): List<ContractDbDto>? = contractDao.getExpiringContractsIn(today,  maxTime)
+    fun getExpiringContractsIn(today: Long, maxTime: Long): List<BaseContract>? = contractDao.getExpiringContractsIn(today,  maxTime)
     fun numberOfContractsExpiring(today: Long, maxTime: Long): Int = contractDao.numberOfContractsExpiring(today,  maxTime)
 }
