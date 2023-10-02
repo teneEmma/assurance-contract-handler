ad2d1663835582ff4de59761574ffe1dd1005ab8
TENE Emma <tenetampo@spreeloop.com>
2023-10-02 23:26:08 +0100

    reformatting code and util to format date gotten from excel file into locale date

--- app/src/main/java/com/kod/assurancecontracthandler/common/utilities/TimeConverters.kt
+++ app/src/main/java/com/kod/assurancecontracthandler/common/utilities/TimeConverters.kt
@@ -1,8 +1,6 @@
 package com.kod.assurancecontracthandler.common.utilities
 
 import com.kod.assurancecontracthandler.common.constants.ConstantsVariables
-import java.text.SimpleDateFormat
-import java.util.*
 
 // TODO: Document this function.
 class TimeConverters {
@@ -52,25 +50,16 @@ class TimeConverters {
      *
      */
     private fun dateFromFormattedString(str: String): String? {
-        val dateRegex = Regex(
-            "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\\1|" +
-                    "(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\\2))" +
-                    "(?:(?:1[6-9]|[2-9]\\d)?\\d{2})\$|^(?:29(\\/|-|\\.)(?:0?2|(?:Feb))\\3(?:(?:(?:1[6-9]|[2-9]\\d)" +
-                    "?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))\$|^(?:0?[1-9]|1\\d|2[0-8])" +
-                    "(\\/|-|\\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\\4" +
-                    "(?:(?:1[6-9]|[2-9]\\d)?\\d{2})\$"
-        )
-        if (!dateRegex.matches(str.trim())) {
+        val dateRegex = Regex("^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\\1|" +
+                "(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\\2))" +
+                "(?:(?:1[6-9]|[2-9]\\d)?\\d{2})\$|^(?:29(\\/|-|\\.)(?:0?2|(?:Feb))\\3(?:(?:(?:1[6-9]|[2-9]\\d)" +
+                "?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))\$|^(?:0?[1-9]|1\\d|2[0-8])" +
+                "(\\/|-|\\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\\4" +
+                "(?:(?:1[6-9]|[2-9]\\d)?\\d{2})\$")
+        if(!dateRegex.matches(str.trim())){
             return null
         }
         return str
     }
 
-    fun formatISODateToLocaleDate(dateToFormat: String): String? {
-        val isoFormat = SimpleDateFormat(ConstantsVariables.isoDatePattern, Locale("en"))
-        val isoDate = isoFormat.parse(dateToFormat)
-        val formattedDate = SimpleDateFormat(ConstantsVariables.desiredDatePattern, Locale("en"))
-
-        return isoDate?.let { formattedDate.format(it) }
-    }
 }
