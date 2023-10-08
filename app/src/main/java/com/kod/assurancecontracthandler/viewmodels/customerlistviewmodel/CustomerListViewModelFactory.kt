e29aa92c84ba3e3efc650729800edba5d1ebec17
TENE Emma <tenetampo@spreeloop.com>
2023-10-08 19:40:38 +0100

    customer viewmodel

--- app/src/main/java/com/kod/assurancecontracthandler/viewmodels/customerlistviewmodel/CustomerListViewModelFactory.kt
+++ app/src/main/java/com/kod/assurancecontracthandler/viewmodels/customerlistviewmodel/CustomerListViewModelFactory.kt
@@ -2,11 +2,13 @@ package com.kod.assurancecontracthandler.viewmodels.customerlistviewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.kod.assurancecontracthandler.repository.ContractRepository
+import com.kod.assurancecontracthandler.repository.CustomerRepository
 
-class CustomerListViewModelFactory(val repository: ContractRepository): ViewModelProvider.Factory {
+class CustomerListViewModelFactory(private val customerRepository: CustomerRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if(modelClass.isAssignableFrom(DatabaseViewModel::class.java)) return DatabaseViewModel(repository) as T
+        if (modelClass.isAssignableFrom(CustomerListViewModel::class.java)) return CustomerListViewModel(
+            customerRepository
+        ) as T
         throw java.lang.IllegalArgumentException("Unknown ViewModel Class")
     }
 }
