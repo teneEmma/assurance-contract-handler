2cb32449258000b3869a5f41aef6ca6b8e444da7
TENE Emma <tenetampo@spreeloop.com>
2023-10-10 23:18:55 +0100

    updated models

--- app/src/main/java/com/kod/assurancecontracthandler/model/Customer.kt
+++ app/src/main/java/com/kod/assurancecontracthandler/model/Customer.kt
@@ -12,43 +12,24 @@ data class Customer(
     @ColumnInfo(name = "assure") var customerName: String?,
     @ColumnInfo(name = "telephone") var phoneNumber: String?
 ) : Parcelable{
-    @Ignore var carteRose: String? = null
-    @Ignore var immatriculation: String? = null
-    @Ignore var numeroPolice: String? = null
-    @Ignore var mark: String? = null
-    @Ignore var effet: Long? = null
-    @Ignore var echeance: Long?= null
+    // The contract number sent when trying to open a customer page to view the details of a contract
+    @Ignore var actualContractNumber: Int? = null
 
     companion object : Parceler<Customer> {
         override fun Customer.write(parcel: Parcel, flags: Int) {
             parcel.writeString(customerName)
             parcel.writeString(phoneNumber)
-            parcel.writeString(carteRose)
-            parcel.writeString(immatriculation)
-            parcel.writeString(numeroPolice)
-            parcel.writeString(mark)
-            effet?.let { parcel.writeLong(it) }
-            echeance?.let { parcel.writeLong(it) }
+            actualContractNumber?.let { parcel.writeInt(it) }
         }
 
         override fun create(parcel: Parcel): Customer {
             val name = parcel.readString()
             val phoneNumber = parcel.readString()
-            val carteRose = parcel.readString()
-            val attestation = parcel.readString()
-            val numeroPolice = parcel.readString()
-            val mark = parcel.readString()
-            val echeance = parcel.readLong()
-            val effet = parcel.readLong()
+            val actualContractNumber = parcel.readInt()
 
 
             val customer = Customer(name, phoneNumber)
-            customer.carteRose = carteRose
-            customer.immatriculation = attestation
-            customer.numeroPolice = numeroPolice
-            customer.mark = mark
-            customer.effet = effet
-            customer.echeance = echeance
+            customer.actualContractNumber = actualContractNumber
             return customer
         }
     }
