c9a8351148e1991ac0e7558948b54f35a0f4e247
TENE Emma <tenetampo@spreeloop.com>
2023-10-10 23:58:18 +0100

    updated customer repository

--- app/src/main/java/com/kod/assurancecontracthandler/repository/CustomerRepository.kt
+++ app/src/main/java/com/kod/assurancecontracthandler/repository/CustomerRepository.kt
@@ -10,6 +10,15 @@ class CustomerRepository(private val customerDAO: CustomerDAO) {
 
     fun getCustomerPhoneNumbers(name: String) = customerDAO.getCustomersPhoneNumbers(name)
 
+    fun getCustomerWithName(concatenatedName: String) = customerDAO.getCustomerWithName(concatenatedName)
+
+    suspend fun updateCustomer(oldName: String, customerName: String, phoneNumber: String?) =
+        customerDAO.updateCustomer(
+            oldCustomerName = oldName,
+            newCustomerName = customerName,
+            phoneNumber = phoneNumber
+        )
+
     fun numberCustomersWithTelephone() = customerDAO.getNumberOfCustomersWithPhones()
 
     fun getActiveContracts(today: String) = customerDAO.getActiveContracts(today)
