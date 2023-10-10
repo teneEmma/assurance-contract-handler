b86b9ce8667377c269d2bc22caef564380f99ac3
TENE Emma <tenetampo@spreeloop.com>
2023-10-10 23:47:04 +0100

    getting name innitials

--- app/src/main/java/com/kod/assurancecontracthandler/common/utilities/DataTypesConversionAndFormattingUtils.kt
+++ app/src/main/java/com/kod/assurancecontracthandler/common/utilities/DataTypesConversionAndFormattingUtils.kt
@@ -18,6 +18,31 @@ object DataTypesConversionAndFormattingUtils {
         }
     }
 
+    fun setNameInitials(customerName: String?): String {
+        if(customerName == null){
+            return "??"
+        }
+        val initialsList = try {
+            if (customerName.isNotEmpty()) {
+                customerName.split(" ").onEach { it.first() }
+            } else {
+                listOf("???")
+            }
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+            listOf("???")
+        }
+
+        var initials = ""
+        for (name in initialsList) {
+            initials += name.first()
+        }
+        if (initials.length > 3)
+            initials = initials.substring(0, 2)
+
+        return initials
+    }
+
     fun addPowerOrPriceUnitToAttribute(value: String, groupPosition: Int): String{
         return if(groupPosition == 0){
             "${value}${ConstantsVariables.priceUnit}"
