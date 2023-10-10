175e9c2515b5e404d746c2b3dc27966df6252274
TENE Emma <tenetampo@spreeloop.com>
2023-10-10 23:35:22 +0100

    datastore repository

---
+++ app/src/main/java/com/kod/assurancecontracthandler/common/utilities/DataStoreRepository.kt
@@ -0,0 +1,21 @@
+package com.kod.assurancecontracthandler.common.utilities
+
+import android.content.SharedPreferences
+
+class DataStoreRepository(private val sharedPrefs: SharedPreferences){
+    companion object{
+        private const val SHARED_PREFERENCE_MESSAGE = "predefined_message"
+        private const val SHARED_PREFERENCE_NEWLY_INSTALLED = "newly_installed"
+    }
+
+    fun readPredefinedMessage(): String? = sharedPrefs.getString(SHARED_PREFERENCE_MESSAGE, "")
+
+    fun savePredefinedMessage(msg: String) = sharedPrefs.edit().putString(SHARED_PREFERENCE_MESSAGE, msg).apply()
+
+    fun isFirstTime(): Boolean = sharedPrefs.getBoolean(
+        SHARED_PREFERENCE_NEWLY_INSTALLED,
+        true)
+
+    fun setFirstTimeNot() = sharedPrefs.edit().putBoolean(
+        SHARED_PREFERENCE_NEWLY_INSTALLED, false).apply()
+}
