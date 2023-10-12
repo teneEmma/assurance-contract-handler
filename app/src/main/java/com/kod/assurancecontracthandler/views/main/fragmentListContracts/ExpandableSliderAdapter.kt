d5752ecaff1ef2209f84fc34e49a11d89bcc667c
Tene Tampo Emmanuel <103233184+teneEmma@users.noreply.github.com>
2023-10-12 22:59:25 +0100

    constant variables (#129)
    * constant variables
    
    * merge from master

--- app/src/main/java/com/kod/assurancecontracthandler/views/main/fragmentListContracts/ExpandableSliderAdapter.kt
+++ app/src/main/java/com/kod/assurancecontracthandler/views/main/fragmentListContracts/ExpandableSliderAdapter.kt
@@ -6,83 +6,85 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseExpandableListAdapter
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.view.isVisible
-import com.google.android.material.slider.RangeSlider
 import com.kod.assurancecontracthandler.R
 import com.kod.assurancecontracthandler.common.constants.ConstantsVariables
+import com.kod.assurancecontracthandler.common.utilities.DataTypesConversionAndFormattingUtils
 import com.kod.assurancecontracthandler.databinding.ExpandableSliderItemBinding
+import kotlin.math.roundToInt
 
 class ExpandableSliderAdapter(
     private val context: Context,
     private val groupTitleList: List<String>,
     private val childrenTitleList: List<List<String>>,
     private val slidersValues: Map<String, Map<String, Pair<Float, Float>?>>,
-    private val sliderListenerCallback: (Pair<Int, Int>, Pair<Float, Float>) -> Unit
+    private val sliderListenerCallback: (Pair<Int, Int>, Pair<Float, Float>) -> Unit,
 ) : BaseExpandableListAdapter() {
 
     private val childrenSelected: MutableList<MutableList<Int>> =
         mutableListOf(mutableListOf(), mutableListOf(), mutableListOf())
+    private var _stepSizeForPrices: Float = 1000F
+    private var _stepSizeForPower: Float = 1F
+    private var _stepSizeForTime: Float = 1F
 
     private fun setSliderValues(
-        groupPosition: Int,
-        childPosition: Int,
-        expandableSBinding: ExpandableSliderItemBinding
+        groupPosition: Int, childPosition: Int, expandableSBinding: ExpandableSliderItemBinding
     ) {
         val groupKey = groupTitleList[groupPosition]
         val childKey = childrenTitleList[groupPosition][childPosition]
         when (groupPosition) {
             0 -> {
-                val minValue = slidersValues[groupKey]?.get(childKey)?.first ?: 1000.0F
-                val maxValue = slidersValues[groupKey]?.get(childKey)?.second ?: 500000.0F
-                expandableSBinding.priceSlider.valueFrom = 1000.0F
-                expandableSBinding.priceSlider.valueTo = 500000.0F
-                expandableSBinding.priceSlider.stepSize = 1000.0F
+                val minValue =
+                    slidersValues[groupKey]?.get(childKey)?.first ?: ConstantsVariables.minPriceValue.toFloat()
+                val maxValue =
+                    slidersValues[groupKey]?.get(childKey)?.second ?: ConstantsVariables.maxPriceValue.toFloat()
+                expandableSBinding.priceSlider.valueFrom = ConstantsVariables.minPriceValue.toFloat()
+                expandableSBinding.priceSlider.valueTo = ConstantsVariables.maxPriceValue.toFloat()
+                expandableSBinding.priceSlider.stepSize = _stepSizeForPrices
                 expandableSBinding.priceSlider.values = mutableListOf(minValue, maxValue)
                 expandableSBinding.priceSlider.setLabelFormatter { value ->
                     "$value${ConstantsVariables.priceUnit}"
                 }
-                expandableSBinding.etPriceRangeMax.text = ConstantsVariables.maxPriceText
-                expandableSBinding.etPriceRangeMin.text = ConstantsVariables.minPriceText
+                setTextViewValues(expandableSBinding, minValue, maxValue, groupPosition)
             }
 
             1 -> {
-                val minValue = slidersValues[groupKey]?.get(childKey)?.first ?: 1.0F
-                val maxValue = slidersValues[groupKey]?.get(childKey)?.second ?: 30.0F
-                expandableSBinding.priceSlider.valueFrom = 1.0F
-                expandableSBinding.priceSlider.valueTo = 30.0F
-                expandableSBinding.priceSlider.stepSize = 1.0F
+                val minValue =
+                    slidersValues[groupKey]?.get(childKey)?.first ?: ConstantsVariables.minPowerValue.toFloat()
+                val maxValue =
+                    slidersValues[groupKey]?.get(childKey)?.second ?: ConstantsVariables.maxPowerValue.toFloat()
+                expandableSBinding.priceSlider.valueFrom = ConstantsVariables.minPowerValue.toFloat()
+                expandableSBinding.priceSlider.valueTo = ConstantsVariables.maxPowerValue.toFloat()
+                expandableSBinding.priceSlider.stepSize = _stepSizeForPower
                 expandableSBinding.priceSlider.values = mutableListOf(minValue, maxValue)
                 expandableSBinding.priceSlider.setLabelFormatter { value ->
                     "$value${ConstantsVariables.powerUnit}"
                 }
-                expandableSBinding.etPriceRangeMax.text = ConstantsVariables.maxPowerText
-                expandableSBinding.etPriceRangeMin.text = ConstantsVariables.minPowerText
+                setTextViewValues(expandableSBinding, minValue, maxValue, groupPosition)
             }
 
             2 -> {
-                val minValue = slidersValues[groupKey]?.get(childKey)?.first ?: 0.0F
-                val maxValue = slidersValues[groupKey]?.get(childKey)?.second ?: 365.0F
-                expandableSBinding.priceSlider.valueFrom = 0.0F
-                expandableSBinding.priceSlider.valueTo = 365.0F
-                expandableSBinding.priceSlider.stepSize = 1.0F
+                val minValue =
+                    slidersValues[groupKey]?.get(childKey)?.first ?: ConstantsVariables.minTimeValue.toFloat()
+                val maxValue =
+                    slidersValues[groupKey]?.get(childKey)?.second ?: ConstantsVariables.maxTimeValue.toFloat()
+                expandableSBinding.priceSlider.valueFrom = ConstantsVariables.minTimeValue.toFloat()
+                expandableSBinding.priceSlider.valueTo = ConstantsVariables.maxTimeValue.toFloat()
+                expandableSBinding.priceSlider.stepSize = _stepSizeForTime
                 expandableSBinding.priceSlider.values = mutableListOf(minValue, maxValue)
                 expandableSBinding.priceSlider.setLabelFormatter { value ->
                     "$value${ConstantsVariables.timeUnit}"
                 }
-                expandableSBinding.etPriceRangeMax.text = ConstantsVariables.maxDurationText
-                expandableSBinding.etPriceRangeMin.text = ConstantsVariables.minDurationText
+                setTextViewValues(expandableSBinding, minValue, maxValue, groupPosition)
             }
         }
 
     }
 
     override fun getChildView(
-        groupPosition: Int,
-        childPosition: Int,
-        isLastChild: Boolean,
-        convertView: View?,
-        parent: ViewGroup?
+        groupPosition: Int, childPosition: Int, isLastChild: Boolean, convertView: View?, parent: ViewGroup?
     ): View {
         var myConvertView = convertView
         val expandedListText = getChild(groupPosition, childPosition) as String
@@ -99,32 +101,44 @@ class ExpandableSliderAdapter(
             Log.e("CRASH_SLIDERS", e.stackTraceToString())
         }
 
-        Log.e("WTF", "OOOOOOOO  $groupTitleList | $childrenTitleList")
-        Log.e("WTF", "XXXXXXXXX  $groupPosition | $childPosition")
-        expandableSBinding.llEtPriceRange.visibility =
-            if (!childrenSelected.get(groupPosition).contains(childPosition)) {
+        expandableSBinding.llEtPriceRange.visibility = if (!childrenSelected[groupPosition].contains(childPosition)) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+        myConvertView.setOnClickListener {
+
+            expandableSBinding.llEtPriceRange.visibility = if (expandableSBinding.llEtPriceRange.isVisible) {
                 View.GONE
             } else {
                 View.VISIBLE
             }
-        myConvertView.setOnClickListener {
-
-            expandableSBinding.llEtPriceRange.visibility =
-                if (expandableSBinding.llEtPriceRange.isVisible) {
-                    View.GONE
-                } else {
-                    View.VISIBLE
-                }
 
             expandableSBinding.priceSlider.apply {
-
-                addOnSliderTouchListener(object : RangeSlider.OnSliderTouchListener {
-                    override fun onStartTrackingTouch(slider: RangeSlider) {
-
+                setTextViewValues(
+                    binding = expandableSBinding,
+                    groupPosition = groupPosition,
+                    minValue = values[0],
+                    maxValue = values[1]
+                )
+                addOnChangeListener { slider, _, _ ->
+                    stepSize = when (groupPosition) {
+                        0 -> _stepSizeForPrices
+                        1 -> _stepSizeForPower
+                        else -> _stepSizeForTime
                     }
-
-                    override fun onStopTrackingTouch(slider: RangeSlider) {}
-                })
+                    val minValue = getRoundedValueCorrespondingToStepSize(slider.values[0], stepSize)
+                    val maxValue = getRoundedValueCorrespondingToStepSize(slider.values[1], stepSize)
+                    setTextViewValues(
+                        binding = expandableSBinding,
+                        groupPosition = groupPosition,
+                        minValue = minValue,
+                        maxValue = maxValue
+                    )
+                    sliderListenerCallback(
+                        Pair(groupPosition, childPosition), Pair(minValue, maxValue)
+                    )
+                }
             }
 
             childrenSelected[groupPosition].add(childPosition)
@@ -136,31 +150,51 @@ class ExpandableSliderAdapter(
         return myConvertView
     }
 
-    override fun getGroupCount(): Int =
-        groupTitleList.size
+    /**
+     * Sets the text views for actual minimum and maximum text fields and adds the unit to them.
+     * @param binding The view of the filter dialog
+     * @param minValue The minimum value by of slider.
+     * @param maxValue The maximum value of the slider
+     * @param groupPosition The group position of the slider. In order to know the unit to add
+     */
+    private fun setTextViewValues(
+        binding: ExpandableSliderItemBinding, minValue: Float, maxValue: Float, groupPosition: Int
+    ) {
+        binding.priceSlider.values = mutableListOf(minValue, maxValue)
+        val minValueString = DataTypesConversionAndFormattingUtils.addPowerOrPriceUnitToAttribute(
+            minValue.toInt(), groupPosition
+        )
+        val maxValueString = DataTypesConversionAndFormattingUtils.addPowerOrPriceUnitToAttribute(
+            maxValue.toInt(), groupPosition
+        )
+        binding.etPriceRangeMin.text = minValueString
+        binding.etPriceRangeMax.text = maxValueString
+    }
 
-    override fun getChildrenCount(groupPosition: Int): Int =
-        childrenTitleList[groupPosition].size
+    private fun getRoundedValueCorrespondingToStepSize(value: Float?, stepSize: Float): Float {
+        if (value == null) {
+            return 0F
+        }
+        val roundedValue: Int = (value / stepSize).roundToInt() * stepSize.toInt()
+        return roundedValue.toFloat()
+    }
 
-    override fun getGroup(groupPosition: Int): Any =
-        groupTitleList[groupPosition]
+    override fun getGroupCount(): Int = groupTitleList.size
 
-    override fun getChild(groupPosition: Int, childPosition: Int): Any =
-        childrenTitleList[groupPosition][childPosition]
+    override fun getChildrenCount(groupPosition: Int): Int = childrenTitleList[groupPosition].size
 
-    override fun getGroupId(groupPosition: Int): Long =
-        groupPosition.toLong()
+    override fun getGroup(groupPosition: Int): Any = groupTitleList[groupPosition]
 
-    override fun getChildId(groupPosition: Int, childPosition: Int): Long =
-        childPosition.toLong()
+    override fun getChild(groupPosition: Int, childPosition: Int): Any = childrenTitleList[groupPosition][childPosition]
+
+    override fun getGroupId(groupPosition: Int): Long = groupPosition.toLong()
+
+    override fun getChildId(groupPosition: Int, childPosition: Int): Long = childPosition.toLong()
 
     override fun hasStableIds(): Boolean = false
 
     override fun getGroupView(
-        groupPosition: Int,
-        isExpanded: Boolean,
-        convertView: View?,
-        parent: ViewGroup?
+        groupPosition: Int, isExpanded: Boolean, convertView: View?, parent: ViewGroup?
     ): View {
         var myConvertView = convertView
         if (myConvertView == null) {
@@ -169,8 +203,117 @@ class ExpandableSliderAdapter(
         }
 
         val groupListTextView = myConvertView?.findViewById<TextView>(R.id.textView3)
+        myConvertView?.findViewById<ImageView>(R.id.iv_btn_add)?.setOnClickListener {
+            onStepSizeIncreased(groupPosition)
+        }
+
+        myConvertView?.findViewById<ImageView>(R.id.iv_btn_reduce)?.setOnClickListener {
+            onStepSizeDecreased(groupPosition)
+        }
         groupListTextView?.text = getGroup(groupPosition) as String
         return myConvertView!!
+    }
+
+    private fun onStepSizeIncreased(groupPosition: Int) {
+        when (groupPosition) {
+            0 -> {
+                val value: Float = getNextStepSizeForDesiredNumber(
+                    actualStepSize = _stepSizeForPrices.toInt(),
+                    desiredNumber = ConstantsVariables.maxPriceValue,
+                    multiple = 1000
+                )
+                if (value <= ConstantsVariables.maxPriceValue) {
+                    _stepSizeForPrices = value
+                }
+            }
+
+            1 -> {
+                val value: Float = getNextStepSizeForDesiredNumber(
+                    actualStepSize = _stepSizeForPower.toInt(),
+                    desiredNumber = ConstantsVariables.maxPowerValue,
+                    multiple = 5
+                )
+                if (value <= ConstantsVariables.maxPowerValue) {
+                    _stepSizeForPower = value
+                }
+            }
+
+            else -> {
+                val value: Float = getNextStepSizeForDesiredNumber(
+                    actualStepSize = _stepSizeForTime.toInt(),
+                    desiredNumber = ConstantsVariables.maxTimeValue,
+                    multiple = 5
+                )
+                if (value <= ConstantsVariables.maxTimeValue) {
+                    _stepSizeForTime = value
+                }
+            }
+        }
+    }
+
+    private fun onStepSizeDecreased(groupPosition: Int) {
+        when (groupPosition) {
+            0 -> {
+                val value: Float = getPrecedentStepSizeForDesiredNumber(
+                    actualStepSize = _stepSizeForPrices.toInt(),
+                    desiredNumber = ConstantsVariables.maxPriceValue,
+                    multiple = 5000
+                )
+                if (value <= ConstantsVariables.maxPriceValue) {
+                    _stepSizeForPrices = value
+                }
+            }
+
+            1 -> {
+                val value: Float = getPrecedentStepSizeForDesiredNumber(
+                    actualStepSize = _stepSizeForPower.toInt(),
+                    desiredNumber = ConstantsVariables.maxPowerValue,
+                    multiple = 5
+                )
+                if (value <= ConstantsVariables.maxPowerValue) {
+                    // Removing unwanted non multiples of 5
+                    _stepSizeForPower = value
+                }
+            }
+
+            else -> {
+                val value: Float = getPrecedentStepSizeForDesiredNumber(
+                    actualStepSize = _stepSizeForTime.toInt(),
+                    desiredNumber = ConstantsVariables.maxTimeValue,
+                    multiple = 5
+                )
+                if (value <= ConstantsVariables.maxTimeValue) {
+                    // Removing unwanted non multiples of 5
+                    _stepSizeForTime = value
+                }
+            }
+        }
+    }
+
+    private fun getNextStepSizeForDesiredNumber(actualStepSize: Int, desiredNumber: Int, multiple: Int): Float {
+        // Removing unwanted non multiples of 5
+        val stepSize = (actualStepSize - actualStepSize % multiple) + multiple
+        if (stepSize >= desiredNumber) {
+            return stepSize.toFloat()
+        }
+        return if (desiredNumber % stepSize != 0) {
+            getNextStepSizeForDesiredNumber(stepSize, desiredNumber, multiple)
+        } else {
+            stepSize.toFloat()
+        }
+    }
+
+    private fun getPrecedentStepSizeForDesiredNumber(actualStepSize: Int, desiredNumber: Int, multiple: Int): Float {
+        // Removing unwanted non multiples of 5
+        val stepSize = (actualStepSize - actualStepSize % multiple) - multiple
+        if (stepSize <= 1) {
+            return 1F
+        }
+        return if (desiredNumber % stepSize != 0) {
+            getPrecedentStepSizeForDesiredNumber(stepSize, desiredNumber, multiple)
+        } else {
+            stepSize.toFloat()
+        }
     }
 
     override fun isChildSelectable(groupPosition: Int, childPosition: Int): Boolean = true
