da3577ba87b99b223520a254232fd4c88583efa3
TENE Emma <tenetampo@spreeloop.com>
2023-10-17 21:07:33 +0100

    expiration contract viewmodel

--- app/src/main/java/com/kod/assurancecontracthandler/viewmodels/expirationworkerviewmodel/ExpirationWorkerViewModelFactory.kt
+++ app/src/main/java/com/kod/assurancecontracthandler/viewmodels/expirationworkerviewmodel/ExpirationWorkerViewModelFactory.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.kod.assurancecontracthandler.repository.ContractRepository
 
-class SelectFileViewModelFactory(private val contractRepository: ContractRepository): ViewModelProvider.Factory {
+class ExpirationWorkerViewModelFactory(private val contractRepository: ContractRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if(modelClass.isAssignableFrom(SelectFileViewModel::class.java)) return SelectFileViewModel(contractRepository) as T
+        if (modelClass.isAssignableFrom(ExpirationWorkerViewModel::class.java)) return ExpirationWorkerViewModel(
+            contractRepository
+        ) as T
         throw java.lang.IllegalArgumentException("Unknown ViewModel Class")
     }
 }
