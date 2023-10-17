848c29facd633a5c6f7184672b79d527dc975f9a
Tene Tampo Emmanuel <103233184+teneEmma@users.noreply.github.com>
2023-10-17 21:56:19 +0100

    Importing from base branch 10 (#151)
    * internalization for notifications
    
    * dependencies for notifications
    
    * no action bar theme for expiring contract activity
    
    * no action bar theme for expiring contract activity
    
    * contant variables
    
    * updated repositories and dao
    
    * expiration contract viewmodel
    
    * work managers
    
    * appApplication and database viewmodel

---
+++ app/src/main/java/com/kod/assurancecontracthandler/common/workmanager/ExpirationWorker.kt
@@ -0,0 +1,75 @@
+package com.kod.assurancecontracthandler.common.workmanager
+
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.kod.assurancecontracthandler.R
+import com.kod.assurancecontracthandler.common.constants.ConstantsVariables
+import com.kod.assurancecontracthandler.common.utilities.TimeConverters
+import com.kod.assurancecontracthandler.model.database.ContractDatabase
+import com.kod.assurancecontracthandler.repository.ContractRepository
+import com.kod.assurancecontracthandler.viewmodels.selectfileviewmodel.ExpirationWorkerViewModel
+import com.kod.assurancecontracthandler.views.expiringactivity.ExpiringContractsActivity
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+
+open class ExpirationWorker(val context: Context, workerParams: WorkerParameters) :
+    CoroutineWorker(context, workerParams) {
+    override suspend fun doWork(): Result {
+        checkExpiringContracts()
+        return Result.success()
+    }
+
+    private fun checkExpiringContracts() {
+        val contractRepository = ContractRepository(ContractDatabase.getDatabase(context).contractDao())
+        val contractViewModel = ExpirationWorkerViewModel(contractRepository)
+
+        val todayInLong = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC) * 1000
+        val todayString = TimeConverters.formatLongToLocaleDate(todayInLong)
+        val maxDateInLong = LocalDateTime.now().plusMonths(ConstantsVariables.maxMonthForExpiringContracts)
+            .toEpochSecond(ZoneOffset.UTC) * 1000
+        val maxDateString = TimeConverters.formatLongToLocaleDate(maxDateInLong)
+
+        if (todayString == null || maxDateString == null) {
+            return
+        }
+
+        contractViewModel.executeFunctionWithoutAnimation {
+            if (contractViewModel.isContractsExpiring(todayString, maxDateString)) {
+                setupNotification()
+            }
+        }
+    }
+
+    open fun setupNotification() {
+
+        val intent = Intent(context, ExpiringContractsActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            context, 0,
+            intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val builder = NotificationCompat.Builder(context, ConstantsVariables.EXPIRY_CHANNEL_ID_STRING)
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setContentTitle(context.resources.getString(R.string.expiring_notification_title))
+            .setContentText(context.getString(R.string.expiration_notification_message))
+            .setStyle(NotificationCompat.BigTextStyle())
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .setChannelId(ConstantsVariables.EXPIRY_CHANNEL_ID_STRING)
+            .addAction(R.drawable.ic_eye, context.getString(R.string.see_text), pendingIntent)
+            .setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
+
+        NotificationManagerCompat.from(context).apply {
+            notify(ConstantsVariables.EXPIRY_CHANNEL_ID_STRING, ConstantsVariables.expiryChannelID_int, builder.build())
+        }
+    }
+}
---
+++ app/src/main/java/com/kod/assurancecontracthandler/common/workmanager/FirstUsageWorker.kt
@@ -0,0 +1,31 @@
+package com.kod.assurancecontracthandler.common.workmanager
+
+import android.content.Context
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.work.WorkerParameters
+import com.kod.assurancecontracthandler.R
+import com.kod.assurancecontracthandler.common.constants.ConstantsVariables
+
+class FirstUsageWorker(context: Context, params: WorkerParameters) : ExpirationWorker(context, params) {
+    override suspend fun doWork(): Result {
+        setupNotification()
+        return Result.success()
+    }
+
+    override fun setupNotification() {
+        val builder = NotificationCompat.Builder(context, ConstantsVariables.FIRST_USAGE_CHANNEL_ID_STRING)
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setContentTitle(context.resources.getString(R.string.welcome_notification_title))
+            .setContentText(context.getString(R.string.welcome_notification_message))
+            .setStyle(NotificationCompat.BigTextStyle())
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setAutoCancel(true)
+            .setChannelId(ConstantsVariables.FIRST_USAGE_CHANNEL_ID_STRING)
+            .setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
+
+        NotificationManagerCompat.from(context).apply{
+            notify(ConstantsVariables.FIRST_USAGE_CHANNEL_ID_STRING, ConstantsVariables.firstTimeChannelID_int, builder.build())
+        }
+    }
+}
