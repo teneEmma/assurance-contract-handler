13f372e1e9369559725c42806030a7138db6b089
TENE Emma <tenetampo@spreeloop.com>
2023-10-20 23:53:39 +0100

    removed log

--- app/src/main/java/com/kod/assurancecontracthandler/views/main/fragmentFileSelection/SelectFileFragment.kt
+++ app/src/main/java/com/kod/assurancecontracthandler/views/main/fragmentFileSelection/SelectFileFragment.kt
@@ -12,7 +12,6 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.OpenableColumns
 import android.provider.Settings
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
